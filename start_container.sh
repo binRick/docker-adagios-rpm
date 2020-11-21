c8c16aec1c08b3c46f371adcf72f85cae8bc5c2d
root <admin@vpntech.net>
2020-11-21 18:29:57 -0500

    m

--- start_container.sh
+++ start_container.sh
@@ -1,9 +1,13 @@
 PORT=7125
 
 cmd="sudo podman run \
+    --name adagios \
     --security-opt label=type:spc_t \
     --privileged=true \
-    --rm --name adagios -p $PORT:80 -d adagios_systemd_image"
+    --rm \
+    -p $PORT:80 \
+    -p 5000:5000 \
+    -d adagios_systemd_image"
 
 #    --tmpfs /tmp \
 #    --userns=keep-id \
