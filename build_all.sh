47c0cab3643c74874d9e8b7ab51b50845280734c
root <admin@vpntech.net>
2020-11-21 18:03:31 -0500

    m

--- build_all.sh
+++ build_all.sh
@@ -1,2 +1,1 @@
-./render_templates.sh
 ./build_image_base.sh && ./build_image.sh && ./reload_container.sh
--- build_image_base.sh
+++ build_image_base.sh
@@ -1,2 +1,3 @@
+./render_templates.sh
 time sudo podman build -t adagios_systemd_image_base -f .Dockerfile.base $@
 
