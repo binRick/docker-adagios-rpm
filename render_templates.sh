01209b125afe0efe977a5a0a1573b4f27a7061f4
root <admin@vpntech.net>
2020-11-22 03:34:04 -0500

    m

--- render_templates.sh
+++ render_templates.sh
@@ -2,7 +2,6 @@
 set -e
 source setup.sh
 source .venv/bin/activate
-data_file=data.yaml
 get_templates(){
   find configs -name "*.j2"
 }
@@ -12,12 +11,12 @@ mkdir configs/rendered
 
 j2_args="-f yaml -e env"
 
-j2 $j2_args -o .Dockerfile Dockerfile.j2 $data_file
-j2 $j2_args -o .Dockerfile.base Dockerfile.base.j2 $data_file
+j2 $j2_args -o .Dockerfile Dockerfile.j2 $LOGIC_FILE
+j2 $j2_args -o .Dockerfile.base Dockerfile.base.j2 $LOGIC_FILE
 
 get_templates | while read -r template_file; do
   of="configs/rendered/$(echo -e $template_file|sed 's/\.j2$//g'|sed 's/configs\///g')"
-  cmd="j2 -f yaml -o '$of' '$template_file' $data_file"
+  cmd="j2 -f yaml -o '$of' '$template_file' $LOGIC_FILE"
   echo $cmd
   eval $cmd
 done
