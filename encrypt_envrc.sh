e3e8f73854bedd4c59ed2066ee4aa1a09c46b983
root <admin@vpntech.net>
2020-11-21 23:26:29 -0500

    m

--- encrypt_envrc.sh
+++ encrypt_envrc.sh
@@ -5,3 +5,9 @@ safeu --encrypt .envrc --output .envrc.enc
 cat .envrc.enc | base64  > .envrc.enc.base64
 md5sum .envrc*
 ls -altr .envrc*
+
+
+file .envrc | grep -q ASCII || exit 1
+
+git commit .envrc -m 'env updated'
+git push
