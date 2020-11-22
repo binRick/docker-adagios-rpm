91af90500dd604afba6c486eada31f7b374cf41a
root <admin@vpntech.net>
2020-11-22 00:30:04 -0500

    m

--- build_image.sh
+++ build_image.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
 set -e
+eval $(cat setup.sh)
 ./render_templates.sh
 
-time sudo podman build -t adagios_systemd_image -f .Dockerfile $@
+time sudo podman build -t adagios_systemd_image -f .Dockerfile 
+
+set +e
