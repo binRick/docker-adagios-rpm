f96edf6f6296e5262c735c5a340d63801e600296
root <admin@vpntech.net>
2020-11-22 04:17:30 -0500

    m

--- render_templates.sh
+++ render_templates.sh
@@ -13,7 +13,7 @@ j2_args="-f yaml -eenv"
 
 j2 $j2_args -o .Dockerfile Dockerfile.j2 $LOGIC_FILE
 j2 $j2_args -o .Dockerfile.base Dockerfile.base.j2 $LOGIC_FILE
-j2 $j2_args -o .haproxy.cfg configs/haproxy.cfg.j2 $LOGIC_FILE
+j2 $j2_args -o .haproxy.cfg haproxy.cfg.j2 $LOGIC_FILE
 
 get_templates | while read -r template_file; do
   of="configs/rendered/$(echo -e $template_file|sed 's/\.j2$//g'|sed 's/configs\///g')"
