a2dd5576d0be84e7faa31f2c2b2550f83842be01
root <admin@vpntech.net>
2020-11-22 06:51:38 -0500

    m

--- files/install_borg.sh
+++ files/install_borg.sh
@@ -2,6 +2,13 @@
 set -e
 [[ -f /opt/borg/bin/activate ]] || virtualenv --python=python3 /opt/borg
 source /opt/borg/bin/activate
-pip install borgbackup
+pip install borgbackup --upgrade
+
+/opt/borg/bin/borg --version
+
+deactivate
+
+/opt/borg/bin/borg --version
 
 
+sh -ilc '/opt/borg/bin/borg --version'
