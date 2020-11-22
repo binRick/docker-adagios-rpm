b5a81c5cf0182997888dddbaf97143f02b68af7e
root <admin@vpntech.net>
2020-11-22 11:15:50 -0500

    m

--- bin/development_tunnels.sh
+++ bin/development_tunnels.sh
@@ -16,10 +16,12 @@ for index in $(seq 1 $MAX_DEV_DOMAIN_INDEX); do
   DEV_IP="${!varname}"
   varname="DEV_DOMAIN_${index}_ADAGIOS_PORT"
   ADG_PORT="${!varname}"
+  varname="DEV_DOMAIN_${index}_HAPROXY_PORT"
+  TTYD_PORT="${!varname}"
   DEV_URL_1="http://$DEV_DOMAIN_USERNAME:$DEV_DOMAIN_PASSWORD@localhost:${index}${DEV_PORT_1}/adagios/status"
   line_break="echo -e \\\"\\\n\\\""
   netstat_cmd="netstat -alntp|egrep \\\":$MY_ADAGIOS_PORT|:$DEV_PORT_2\\\"|head -n5"
-  ssh_args="-tt -oStrictHostKeyChecking=no -oLogLevel=ERROR -oControlMaster=no -L ${index}$DEV_PORT_2:127.0.0.1:$DEV_PORT_2 -L ${index}$DEV_PORT_1:127.0.0.1:$ADG_PORT"
+  ssh_args="-tt -oStrictHostKeyChecking=no -oLogLevel=ERROR -oControlMaster=no -L ${index}$DEV_PORT_2:127.0.0.1:$TTYD_PORT -L ${index}$DEV_PORT_1:127.0.0.1:$ADG_PORT"
   remote_cmd="bash -c \"while [[ 1 ]]; do clear; hostname -f; $podman_cmd; echo -e \\\"\\\n\\\n$DEV_URL_1\\\n\\\n\\\"; echo -e \\\"\\\n\\\"; $pod_stats_cmd; $netstat_cmd; $line_break; sleep $dur; done\""
   cmd="command ssh -oHostName='$DEV_IP' $ssh_args '$DEV_DOMAIN' '$remote_cmd'"
 
