743a3bbde53e45664c899558da967c9b556d3161
root <admin@vpntech.net>
2020-11-22 10:20:40 -0500

    m

--- local_scripts/get_container_key.sh
+++ local_scripts/get_container_key.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+secret_key_file=.secret-${MY_CONTAINER_1_UUID}.key
+
 get_secret_key(){
-    sudo podman cp $(sudo podman ps -q):/var/lib/thruk/secret.key . && sudo chmod 755 secret.key && cat secret.key && unlink secret.key
+    sudo podman cp $MY_CONTAINER_1_UUID:/var/lib/thruk/secret.key $secret_key_file && sudo chown root:root $secret_key_file && sudo chmod 600 $secret_key_file && sudo cat $secret_key_file && sudo unlink $secret_key_file
 }
+
+
 
 get_secret_key
