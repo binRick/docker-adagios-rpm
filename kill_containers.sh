1297fbaf36e5ecbdc61de3da3c0e0836addf02e6
root <admin@vpntech.net>
2020-11-22 00:54:41 -0500

    m

--- kill_containers.sh
+++ kill_containers.sh
@@ -1,7 +1,9 @@
+set +ex
+
+if [[ "$(sudo podman ps -aq|wc -l)" == "0" ]]; then
+  exit 0
+fi
 #sudo podman kill adagios; sudo podman rm adagios
 #sudo podman ps -qa |xargs sudo podman kill
-(
-  set +e
-  sudo podman ps -aq|xargs sudo podman kill
-  sudo podman ps -aq|xargs sudo podman rm
-) 2>/dev/null
+sudo podman ps -aq|xargs sudo podman kill
+sudo podman ps -aq|xargs sudo podman rm
