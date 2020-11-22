078960458778c16041b7db3fa56b64b09909dab5
root <admin@vpntech.net>
2020-11-21 23:14:08 -0500

    m

--- decrypt_envrc.sh
+++ decrypt_envrc.sh
@@ -2,6 +2,11 @@
 set -ex
 
 cat .envrc.enc.base64 | base64 -d > .envrc.enc
-safeu --decrypt .envrc.enc --output .envrc && direnv allow .
+safeu --decrypt .envrc.enc --output .envrc
+
 md5sum .envrc*
 ls -altr .envrc*
+file .envrc| grep ': data$' -q && { echo Invalid; unlink .envrc; exit 1; }
+
+
+direnv allow .
--- encrypt_envrc.sh
+++ encrypt_envrc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-safeu --encrypt .envrc.enc --output .envrc.enc
+safeu --encrypt .envrc --output .envrc.enc
 cat .envrc.enc | base64  > .envrc.enc.base64
 md5sum .envrc*
 ls -altr .envrc*
