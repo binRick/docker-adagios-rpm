9b499810d116945c55289c982550ae172851b3dd
root <admin@vpntech.net>
2020-11-21 23:53:54 -0500

    m

--- start_pod.sh
+++ start_pod.sh
@@ -6,8 +6,7 @@ my_pod=$MY_UUID
 
 cmd="sudo podman pod create \
     --name $my_pod \
-    -p ${MY_INDEX}$DEV_PORT_1:$DEV_PORT_1 \
-    -p ${MY_INDEX}$DEV_PORT_2:$DEV_PORT_2 \
+    -p $MY_ADAGIOS_PORT:$INTERNAL_ADAGIOS_PORT \
 "
 ./ls_pod.sh || eval $cmd
 
