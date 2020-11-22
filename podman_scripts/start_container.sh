a6afb3bdf956d722cb72938139529e4fab0dc6e4
root <admin@vpntech.net>
2020-11-22 16:47:36 -0500

    madd serviceextinfo post task

--- podman_scripts/start_container.sh
+++ podman_scripts/start_container.sh
@@ -4,6 +4,20 @@ set -e
 source setup.sh
 
 container_hostname="$MY_CONTAINER_1_UUID"
+CPUS_QTY=1
+image_volume=ignore
+image_volume=tmpfs
+image_volume=bind
+privileged=true
+shm_size=64m
+blkio_weight=500   #  10 - 1000
+kernel_memory=200m
+memory=512m
+memory_plus_swap_limit=1024m
+memory_swappiness=50  #  0 - 100
+oom_score_adj=0       #  -1000 - 1000
+pids_limit=500
+add_host_ip="yahoo.com:1.2.3.4"
 
 set +e
 start_pod.sh
@@ -19,16 +33,32 @@ start_named_container_from_image_in_pod(){
 
   cmd="sudo podman run \
     --env CONTAINER_UUID='$MY_CONTAINER_1_UUID' \
-    --shm-size=2048m \
-    --hostname '$container_hostname' \
-    --name '$CONTAINER_NAME' \
+    --cpus='$CPUS_QTY' \
+    --shm-size='$shm_size' \
+    --http-proxy=false \
+    --interactive=false \
+    --kernel-memory='$kernel_memory' \
+    --memory='$memory' \
+    --memory-swap='$memory_plus_swap_limit' \
+    --memory-swappiness='$memory_swappiness' \
+    --image-volume='$image_volume' \
+    --hostname='$container_hostname' \
+    --name='$CONTAINER_NAME' \
+    --oom-kill-disable=false \
+    --pids-limit='$pids_limit' \
+    --oom-score-adj='$oom_score_adj' \
+    --label 'com.example.key=value' \
     --security-opt label=type:spc_t \
-    --privileged=true \
+    --privileged='$privileged' \
     --restart=always \
     --pod='$POD_NAME' \
     --tmpfs /tmp \
     --cap-add=SYS_ADMIN \
+    --blkio-weight='$blkio_weight' \
+    --add-host='$add_host_ip' \
     --userns=keep-id \
+    --env-host=false \
+    --read-only=false \
     -v '/sys/fs/cgroup:/sys/fs/cgroup:ro' \
     -d '$IMAGE_NAME'"
 
--- podman_scripts/start_pod.sh
+++ podman_scripts/start_pod.sh
@@ -4,7 +4,8 @@ set -ex
 
 
 cmd="sudo podman pod create \
-    --name $MY_POD_UUID \
+    --name='$MY_POD_UUID' \
+    --label com.example.key=value \
     -p $MY_ADAGIOS_PORT:$INTERNAL_ADAGIOS_PORT \
     -p $MY_HAPROXY_PORT:$INTERNAL_HAPROXY_PORT \
     -p $MY_HAPROXY_TTYD_NETWORKED_PORT:$INTERNAL_HAPROXY_TTYD_NETWORKED_PORT \
