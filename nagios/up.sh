91d0d6503b541426cb265dd96c9a42081afb5241
root <admin@vpntech.net>
2020-11-24 02:45:41 -0500

    m

--- nagios/up.sh
+++ nagios/up.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 (cd ../ && render_templates.sh)
+
 podman-compose \
     -t publishall \
     -f docker-compose.yaml up
--- repo_cache/setup.sh
+++ repo_cache/setup.sh
@@ -4,22 +4,33 @@ sudo mkdir -p \
     /var/cache/centos/yum/7 \
     /var/cache/centos/yum/8 \
     /var/cache/centos/dnf/7 \
-    /var/cache/centos/dnf/8 
+    /var/cache/centos/dnf/8  \
+    /var/cache/repos/centos/dnf/8 
 
 
-sudo podman run -v /var/cache/centos/yum/7:/var/cache/yum:z -ti centos:7 yum makecache
-sudo podman run -v /var/cache/centos/yum/8:/var/cache/yum:z -ti centos:8 yum makecache
+#sudo podman run --rm -v /var/cache/centos/yum/7:/var/cache/yum:z -ti centos:7 yum makecache
+sudo podman run --rm -v /var/cache/centos/dnf/8:/var/cache/dnf:z -ti centos:8 dnf -y makecache
+#sudo podman run --rm -v /var/cache/centos/yum/8:/var/cache/yum:z -ti centos:8 yum -y makecache
 
-time sudo podman build -v /var/cache/centos/yum/7:/var/cache/yum:O -f Dockerfile_centos_7_yum_update
-time sudo podman build -v /var/cache/centos/yum/8:/var/cache/yum:O -f Dockerfile_centos_8_yum_update
+#time sudo podman build \
+#    -v /var/cache/centos/yum/7:/var/cache/yum:O \
+#        -f Dockerfile_centos_7_yum_update
 
+time sudo podman build \
+    -v /var/cache/centos/dnf/8:/var/cache/dnf:z \
+    -v /var/cache/repos/centos/dnf/8:/var/cache/repo:z \
+        -f Dockerfile_centos_8_yum_update
 
-
-sudo podman run -v /var/cache/centos/dnf/8:/var/cache/dnf:z -ti centos:8 dnf -y makecache
+#time sudo podman run --rm \
+#    -v /var/cache/repos/centos/dnf/8:/var/cache/repo \
+#    -ti centos:8 \
+#        dnf -y reinstall --downloadonly --destdir /var/cache/repo haproxy
 
 
 
 set +x
 find /var/cache/centos/yum/7|wc -l
 find /var/cache/centos/dnf/8|wc -l
+find /var/cache/repos/centos/dnf/8|wc -l
+du --max-depth=1 -h /var/cache/repos/centos/dnf/8
 set -x
