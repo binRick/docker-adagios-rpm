3fa4d3984d5503e4b695a04a8123035a9cec3a01
root <admin@vpntech.net>
2020-11-23 23:06:57 -0500

    m

--- local_scripts/install.sh
+++ local_scripts/install.sh
@@ -6,11 +6,16 @@ pip install pip --upgrade
 pip install \
 	'j2cli[yaml]' \
 	'podman-compose' \
+	'json2yaml' \
 --upgrade
 
 id | grep -q root || sudo -u root pip3 install \
 	'podman-compose' \
+	'json2yaml' \
 --upgrade --user
 
 sudo dnf -y remove docker docker-common
 sudo dnf -y install podman
+systemctl enable io.podman
+systemctl start io.podman
+systemctl status io.podman
