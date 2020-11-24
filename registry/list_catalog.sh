9c5c980454230f5fbbe68ed8b9e6c4f8c0bf963f
root <admin@vpntech.net>
2020-11-23 23:15:56 -0500

    m

---
+++ registry/list_catalog.sh
@@ -0,0 +1,1 @@
+curl -v http://localhost:5009/v2/_catalog
---
+++ registry/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -ex
+
+mkdir -p /opt/registry/{auth,certs,data}
+htpasswd -bBc /opt/registry/auth/htpasswd registryuser registryuserpassword
+
+
+podman run --name myregistry \
+    -p 5009:5000 \
+    -v /opt/registry/data:/var/lib/registry:z \
+    -v /opt/registry/auth:/auth:z \
+    -e "REGISTRY_AUTH=htpasswd" \
+    -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+    -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+    -v /opt/registry/certs:/certs:z \
+    -e "REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt" \
+    -e "REGISTRY_HTTP_TLS_KEY=/certs/domain.key" \
+    -e REGISTRY_COMPATIBILITY_SCHEMA1_ENABLED=true \
+    -d \
+    docker.io/library/registry:latest
+
+
+podman login -u registryuser -p registryuserpassword --tls-verify=false localhost:5009
+
+sudo podman pull registry.access.redhat.com/ubi8/ubi:latest
+podman tag registry.access.redhat.com/ubi8/ubi:latest localhost:5009/ubi8/ubi:latest
+
+podman push localhost:5009/ubi8/ubi:latest --tls-verify=false
+
+
+
