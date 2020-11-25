c0d6c2c86cad8045cfe61d944a3df7fb16e8c5f3
root <admin@vpntech.net>
2020-11-25 18:43:04 -0500

    m

--- pb.sh
+++ pb.sh
@@ -5,3 +5,11 @@ pip3 install ansible --upgrade --user -q
 
 cd ansible-role-podman
 ansible-galaxy install -r ansible-galaxy-requirements.yml
+
+ansible-playbook  -i localhost, pb.yaml -v
+
+which podman
+podman --version
+systemctl status podman
+systemctl is-enabled podman || systemctl enable podman --now
+
