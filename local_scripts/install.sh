427d23bfba1f921ed29e36d6874e7fbc8d6e2009
root <admin@vpntech.net>
2020-11-25 21:23:03 -0500

    m

--- local_scripts/install.sh
+++ local_scripts/install.sh
@@ -11,7 +11,7 @@ pip install \
 	'json2yaml' \
 --upgrade --force
 
-id | grep -q ^root$ || sudo -u root pip3 install \
+id | grep -q ^root$ || sudo -Eu root pip3 install \
 	'pyyaml' \
 	'j2cli[yaml]' \
 	'https://github.com/containers/podman-compose/archive/devel.tar.gz' \
@@ -19,8 +19,8 @@ id | grep -q ^root$ || sudo -u root pip3 install \
 	'json2yaml' \
 --upgrade --user --force
 
-sudo dnf -y remove docker docker-common libselinux-python3
-sudo dnf -y install podman iptables podman-remote podman-docker
-systemctl enable io.podman
-systemctl start io.podman
-systemctl status io.podman
+#sudo dnf -y remove docker docker-common libselinux-python3
+#sudo dnf -y install podman iptables podman-remote podman-docker
+#systemctl enable io.podman
+#systemctl start io.podman
+##systemctl status io.podman
