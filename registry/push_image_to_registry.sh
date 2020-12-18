39b77265250c369c8437e8b45318382f6fc9d5ba
root <admin@vpntech.net>
2020-12-18 02:27:37 -0500

    m

--- registry/push_image_to_registry.sh
+++ registry/push_image_to_registry.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -ex
-
-
+set +e
 podman login -u registryuser -p registryuserpassword --tls-verify=false localhost:5009
 
 set -x
 
-SRC_IMAGE=localhost/nagios-02a53693_centos_7_base_rpms
-DST_IMAGE=nagios-02a53693_centos_7_base_rpms
-
+IMG=vpntech-haproxy-container_haproxy
+SRC_IMAGE=localhost/$IMG
+DST_IMAGE=haproxy-centos-8
 podman tag $SRC_IMAGE localhost:5009/$DST_IMAGE
-
 podman push localhost:5009/$DST_IMAGE --tls-verify=false
 
 
 
+IMG=nagios-02a53693_centos_7
+DST_IMAGE=nagios-centos-7
+SRC_IMAGE=localhost/$IMG
+podman tag $SRC_IMAGE localhost:5009/$DST_IMAGE
+podman push localhost:5009/$DST_IMAGE --tls-verify=false
