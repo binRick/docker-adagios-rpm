14be6c0ef3932189f8bc5514a367476e175009f4
root <root@fedora33-a.vpnservice.company>
2020-12-19 11:30:13 +0000

    m

--- nagios/dev_build.sh
+++ nagios/dev_build.sh
@@ -2,6 +2,9 @@ nodemon \
 	-w container-adagios-compose.yaml \
 	-w fedora_build.sh \
 	-w centos_build/Dockerfile.fedora33 \
+    -w . \
 	-V \
 	-e fedora33,sh,yaml \
+    -i RPM_CACHE/ \
+    --delay .5 \
 	-x time ./build_adagios_image.sh
