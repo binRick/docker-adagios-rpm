e3a470980ddc59ca893042412ec55e2e94c16da4
root <root@fedora33-b.vpnservice.company>
2020-12-19 09:20:53 -0500

    m

--- nagios/build_adagios_image.sh
+++ nagios/build_adagios_image.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 PROJECT_NAME=naemon
-JOBS=1
 bold=$(tput bold)
 red=$(tput setaf 1)
 green=$(tput setaf 2)
@@ -9,9 +8,16 @@ blue=$(tput setaf 4)
 cyan=$(tput setaf 6)
 reset=$(tput sgr0)
 
-echo -e "Building an image called ${PROJECT_NAME}"
+if [[ "$1" == "squash" ]]; then
+  SQUASH="--squash"
+else
+  SQUASH=""
+fi
+JOBS=5
 
+echo -e "Building an image called ${PROJECT_NAME}"
 [[ ! -d RPM_CACHE ]] && mkdir RPM_CACHE
+[[ ! -d /repo ]] && mkdir /repo
 
 stat -c %T -f /sys/fs/cgroup
 
@@ -21,19 +27,26 @@ podman-compose -f container-adagios-compose.yaml down 2>/dev/null
 set -e
 #podman-compose -p $PROJECT_NAME -f container-adagios-compose.yaml up --build --abort-on-container-exit
 #podman build -f centos_build/Dockerfile.fedora33 -t naemon .
-[[ ! -f check-mk-raw-1.6.0p19.cre.tar.gz ]] && wget https://checkmk.com/support/1.6.0p19/check-mk-raw-1.6.0p19.cre.tar.gz
+#[[ ! -f check-mk-raw-1.6.0p19.cre.tar.gz ]] && wget https://checkmk.com/support/1.6.0p19/check-mk-raw-1.6.0p19.cre.tar.gz
 du --max-depth=1 -h RPM_CACHE/
+du --max-depth=1 -h /repo
 
 buildah bud \
+    $SQUASH \
     --jobs $JOBS \
     --volume $(pwd)/RPM_CACHE:/var/cache/dnf:rw,Z \
-    -f centos_build/Dockerfile.fedora33 -t adagios-fedora .
+    --volume /repo:/repo:ro,Z \
+    -f centos_build/Dockerfile.fedora33 \
+    -t adagios-fedora .
 
 echo -e "${green}Image Build is done!${reset}"
 
 buildah images
 
-podman-compose -f container-adagios-compose.yaml  up --no-build --force-recreate --detach
+podman-compose \
+		--project-name $PROJECT_NAME \
+		-f container-adagios-compose.yaml up \
+							--no-build --force-recreate --detach
 
 podman exec nagios_adagios-fedora_1 ps axfuw
 
