f0e3cdbae0b469be7cff5548d7e8ce01634129d8
root <root@fedora33-a.vpnservice.company>
2020-12-19 04:48:52 +0000

    m

--- nagios/build_adagios_image.sh
+++ nagios/build_adagios_image.sh
@@ -9,6 +9,8 @@ podman-compose -f container-adagios-compose.yaml down 2>/dev/null
 set -e
 #podman-compose -p $PROJECT_NAME -f container-adagios-compose.yaml up --build --abort-on-container-exit
 #podman build -f centos_build/Dockerfile.fedora33 -t naemon .
+[[ ! -f check-mk-raw-1.6.0p19.cre.tar.gz ]] && wget https://checkmk.com/support/1.6.0p19/check-mk-raw-1.6.0p19.cre.tar.gz
+
 buildah bud \
     --layers --jobs $JOBS \
     --volume $(pwd)/RPM_CACHE:/var/cache/dnf:rw,Z \
