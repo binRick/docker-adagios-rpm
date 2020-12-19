516cbb7c71ca336947808df3f0e2b4bb918461f8
root <root@fedora33-a.vpnservice.company>
2020-12-19 04:03:06 +0000

    m

--- nagios/build_adagios_image.sh
+++ nagios/build_adagios_image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PROJECT_NAME=naemon
 JOBS=4
+[[ ! -d RPM_CACHE ]] && mkdir RPM_CACHE
 
 set +e
 podman-compose -f container-adagios-compose.yaml down 2>/dev/null
@@ -8,4 +9,7 @@ podman-compose -f container-adagios-compose.yaml down 2>/dev/null
 set -e
 #podman-compose -p $PROJECT_NAME -f container-adagios-compose.yaml up --build --abort-on-container-exit
 #podman build -f centos_build/Dockerfile.fedora33 -t naemon .
-buildah bud --layers --jobs $JOBS -f centos_build/Dockerfile.fedora33 -t adagios-fedora .
+buildah bud \
+    --layers --jobs $JOBS \
+    --volume $(pwd)/RPM_CACHE:/RPM_CACHE:rw,Z \
+    -f centos_build/Dockerfile.fedora33 -t adagios-fedora .
